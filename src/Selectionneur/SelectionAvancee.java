efe11097befe10a2d3a5cf7895bdefe4dfb3e3e6
Mehdi Kaddech <m.kaddech0404@gmail.com>
2026-05-14 22:26:45 +0100

    Update SelectionAvancee.java

--- src/Selectionneur/SelectionAvancee.java
+++ src/Selectionneur/SelectionAvancee.java
@@ -1,29 +1,26 @@
-package Selectionneur;
-import java.util.List;
-import Moteur.ResultatComparaison;
-public  class SelectionAvancee implements Selectionneur{
+package selectionneur;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import model.ResultatComparaison;
+
+public class SelectionAvancee implements Selectionneur {
     public final double seuil;
     public final String sourcePrioritaire;
 
-    public SelectionAvancee(double seuil , String sourcePrioritaire){
-        this.seuil=seuil;
-        this.sourcePrioritaire=sourcePrioritaire;
+    public SelectionAvancee(double seuil, String sourcePrioritaire) {
+        this.seuil = seuil;
+        this.sourcePrioritaire = sourcePrioritaire;
     }
-
-
 
     @Override
-    public List<ResultatComparaison> filtrer(List<ResultatComparaison> resultats) {
-        if (resultats == null) return List.of();
-        return resultats.stream()
+    public List<ResultatComparaison> filtrer(List<ResultatComparaison> resultat) {
+        if (resultat == null) return List.of();
+        return resultat.stream()
                 .filter(r -> r.estAuDessus(seuil))
-                .sorted((a, b) -> {
-                    if (a.source.equals(sourcePrioritaire) != b.source.equals(sourcePrioritaire))
-                        return a.source.equals(sourcePrioritaire) ? -1 : 1;
-                    return Double.compare(b.score, a.score);
-                })
-                .toList();
+                .sorted(Comparator.comparing((ResultatComparaison r) -> !r.source.equals(sourcePrioritaire))
+                        .thenComparing(Comparator.comparingDouble(ResultatComparaison::getScore).reversed()))
+                .collect(Collectors.toList());
     }
-
 }
